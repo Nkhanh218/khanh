d80c7ac042846b94e75617e0f898085bc75768fa
emiliaka2 <20010869@st.phenikaa-uni.edu.vn>
2022-03-03 22:27:32 +0700

    first commit

---
+++ binary search.java
@@ -0,0 +1,32 @@
+// Binary Search in Java
+
+class BinarySearch {
+    int binarySearch(int array[], int x, int low, int high) {
+
+        if (high >= low) {
+            int mid = low + (high - low) / 2;
+
+            if (array[mid] == x)
+                return mid;
+
+            if (array[mid] > x)
+                return binarySearch(array, x, low, mid - 1);
+
+            return binarySearch(array, x, mid + 1, high);
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]) {
+        BinarySearch ob = new BinarySearch();
+        int array[] = { 3, 4, 5, 6, 7, 8, 9 };
+        int n = array.length;
+        int x = 4;
+        int result = ob.binarySearch(array, x, 0, n - 1);
+        if (result == -1)
+            System.out.println("Not found");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}
